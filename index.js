30be009653039bcd7c1c67b7d6ca17ffbcb6ee89
Piush-hexaware <46959519+Piush-hexaware@users.noreply.github.com>
2019-06-10 16:33:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ return res.json(responseObj);
 }
 else if(req.body.queryResult.intent.displayName == "map") {
   console.log("**inside map**" + JSON.stringify(req.body))
-  console.log("%%url%%"+`https://maps.googleapis.com/maps/api/staticmap?center=${req.body.queryResult.parameters.address}&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAWsvXenHXLG_RnVuzls5ZSWVu4InJYYn0`)
+  console.log("%%url%%"+`https://maps.googleapis.com/maps/api/staticmap?center=${req.body.queryResult.parameters.address}&zoom=16&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAWsvXenHXLG_RnVuzls5ZSWVu4InJYYn0`)
   let address = req.body.queryResult.parameters.address.split(" ").join("+")
   coordinate(address)
   .then(body=>{
