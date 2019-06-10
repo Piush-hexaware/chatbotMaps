1b1633d30d86015cc6d285ad9fce6bd8341347ee
Piush Singh <43833@hexaware.com>
2019-06-10 15:08:34 +0530

    inital commit

--- index.js
+++ index.js
@@ -69,14 +69,14 @@ else if(req.body.queryResult.intent.displayName == "map"){
             },
             {
               "basicCard": {
-                "title": "Card Title",
+                "title": `${req.body.queryResult.parameters.address}`,
                 "image": {
-                  "url": "https://maps.googleapis.com/maps/api/staticmap?center=Hexaware+Technologies+chennai&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAWsvXenHXLG_RnVuzls5ZSWVu4InJYYn0",
-                  "accessibilityText": "Google Logo"
+                  "url": `https://maps.googleapis.com/maps/api/staticmap?center=${req.body.queryResult.parameters.address}&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAWsvXenHXLG_RnVuzls5ZSWVu4InJYYn0`,
+                  "accessibilityText": "Google Map"
                 },
                 "buttons": [
                   {
-                    "title": "Button Title",
+                    "title": "Map",
                     "openUrlAction": {
                       "url": "https://www.google.com"
                     }
